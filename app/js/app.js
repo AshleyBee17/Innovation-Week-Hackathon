785ecbf116a5f99ce2d044eb2459c2a9aad9a65f
Adrian Rodriguez <adrianr0120@gmail.com>
2017-06-22 21:08:11 -0400

    fixed anguar bindings and added routers etc...

--- app/js/app.js
+++ app/js/app.js
@@ -31,3 +31,46 @@ $(document).ready(function() {
 		bigSectionsDestination: null
 	});
 });
+
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.controller('mainController', function($scope) {
+
+    // create a message to display in our view
+    $scope.title = 'For Interns, by Interns';
+});
+
+
+myApp.config(function($routeProvider) {
+    $routeProvider
+    .when('/',{
+        templateUrl:'app/views/default/default.html',
+        controller:'mainController'
+    })
+    .when('/home',{
+        templateUrl:'app/views/Home/home.html',
+        controller:'homeController'
+    })
+    .when('/intern101',{
+        templateUrl:'app/views/intern101/intern101.html',
+        controller:'intern101Controller'
+    })
+    .when('/events',{
+        templateUrl:'app/views/events/events.html',
+        controller:'eventsController'
+    });
+});
+
+myApp.controller('homeController', function($scope){
+    $scope.message="this is the home controller";
+})
+
+myApp.controller('intern101Controller', function($scope){
+    $scope.message="this is the 101 controller";
+    $scope.title = "Intern 101";
+})
+
+myApp.controller('eventsController', function($scope){
+    $scope.message="this is the events controller";
+    $scope.title="Events & Calendar";
+})
